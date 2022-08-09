b41a97bd124db4b57d2f26b80d4108147609492b
Zhiqian Zhang <88167567+Zhiqian-Zhang@users.noreply.github.com>
2022-08-08 23:31:59 -0700

    Add files via upload

---
+++ forage-lyft-starter-repo-main/car/battery/spindler_battery.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+from car import Car
+
+
+class SpindlerEngine(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        return self.current_date - self.last_service_date > 2
